9627e4c43703dca4368364f3fb8b25fa1db9e823
Ashish Kumar <ashishsinha0987@gmail.com>
2015-10-09 22:19:20 -0700

    Project Restructure

--- hackathon/src/main/java/org/paypal/hackathon/project/dao/impl/VendorDAOImpl.java
+++ hackathon/src/main/java/org/paypal/hackathon/project/dao/impl/VendorDAOImpl.java
@@ -3,19 +3,21 @@ package org.paypal.hackathon.project.dao.impl;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.paypal.hackathon.project.dao.VendorDAO;
 import org.paypal.hackathon.project.persistence.bean.VendorInformation;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
-public class VendorDAOImpl {
+public class VendorDAOImpl implements VendorDAO {
 
 	@PersistenceContext
 	EntityManager entityManager;
 
+	@Override
 	public VendorInformation fetchSingleVendor() {
-		return this.entityManager.createQuery("from VendorInformation", VendorInformation.class).getSingleResult();
+		return this.entityManager.createQuery("from VendorInformation", VendorInformation.class).getResultList().get(0);
 	}
 
 }
